594f788f3007e4159175ce84c1fcff2e596a0a42
whelixw <andrerosenj@gmail.com>
2025-02-07 18:20:44 +0100

    solved 11_2

--- 11/11_2.py
+++ 11/11_2.py
@@ -1,19 +1,8 @@
-#NOTE: hvis du scaler worry ned, kan du scale summen op tilsvarende.
-# squaring sker kun, hvis tallet er mod 11 eller mod 17
-# a prime squared is divisible by itself, its prime and 1. 11*11 = 121, 121%11 = 0, 121%1 = 0, 121%121 = 0
-# this can be used to reduce the power of the number (at least if it is a prime factor)
-# the modifier is scaled up when the power is reduced, so the sum is still correct
-# the sum can then be scaled down in a similar manner. (I think???)
-
-
-
 import heapq
-
+list_of_tests = []
 def item_transfer(item, Monkey):
     global obj_list
     #print("item", item, "Monkey", Monkey)
-    #if item > 10**9:
-        #print("Monkey", Monkey)
     obj_list[Monkey].item_list.append(item)
 
 class Monkey:
@@ -23,26 +12,32 @@ class Monkey:
         self.test = test
         self.code = compile(self.operation, "<string>", "eval")
         self.friends = friends
-        self.counter = 0
+        self.items_handled = 0
 
 
 
     def run_function(self):
+        global product_of_tests
         while len(self.item_list) > 0:
-            self.item = self.item_list.pop()
-            self.counter += 1
+            self.item = self.item_list.pop(0)
+            self.items_handled += 1
             self.worry = eval(self.code, {"__builtins__": None}, {"old": self.item})
-            #self.worry = int(self.worry/3)
-            if self.worry % self.test == 0:
-                if self.test == 11 or self.test == 17:
-                    modifier = self.test
+            #print(self.worry, "worry")
+            self.worry = self.worry%product_of_tests
+            self.check = self.worry%self.test
+            #print(self.check, "worry2", self.test)
+            if self.check % self.test == 0:
+                #print("true")
                 item_transfer(self.worry, self.friends[0])
             else:
+                #print("false")
                 item_transfer(self.worry, self.friends[1])
+
+
 
 #obj_list = [Monkey([1], "old*3", 4, [1,2]), Monkey([2], "old*5", 7, [2,0]), Monkey([3], "old*7", 9, [0,1])]
 
-input_doc = open("11/input.txt").readlines()
+#input_doc = open("11/input.txt").readlines()
 '''Monkey 0:
   Starting items: 76, 88, 96, 97, 58, 61, 67
   Operation: new = old * 19
@@ -57,12 +52,11 @@ with open("11/input.txt") as f:
         if counter%7 == 1:
             #print(line[18::])
             starting_items = list(map(int, line[18::].split(", ")))
-            # make each item to a list of the item and 1
-            starting_items = [[item, 1] for item in starting_items]
         if counter%7 == 2:
             operation = line[19:-1]
         if counter%7 == 3:
             test = int(line[21:-1])
+            list_of_tests.append(test)
         if counter%7 == 4:
             friend1 = int(line[29:-1])
         if counter%7 == 5:
@@ -72,16 +66,26 @@ with open("11/input.txt") as f:
             obj_list.append(Monkey(starting_items, operation, test, [friend1, friend2]))
         counter += 1
     obj_list.append(Monkey(starting_items, operation, test, [friend1, friend2]))
+product_of_tests = 1
+for test in list_of_tests:
+    product_of_tests *= test
+
 
 for i in range(10000):
     #print(i)
     for index in range(len(obj_list)):
         #print(index)
         obj_list[index].run_function()
+    if i in [1, 20, 1000]:
+        print(i)
+        list_items_handled = []
+        for obj in obj_list:
+            list_items_handled.append(obj.items_handled)
+        print(list_items_handled)
 
 handled_counter_list = []
 for obj in obj_list:
-    handled_counter_list.append(obj.counter)
+    handled_counter_list.append(obj.items_handled)
     #print the highest two values
 largest = heapq.nlargest(2, handled_counter_list)
 print(largest)
